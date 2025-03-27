a8c8db5b6a3a5ddc9f8785476e8577a11c0fefe7
WellingtonPereiraLuiz <wellingtonpereiraluiz89@gmail.com>
2025-03-26 22:14:27 -0300

    One

---
+++ Sala/exerciciioOne.py
@@ -0,0 +1,6 @@
+class Ainimal:
+    def _init_(self, tipo, tamanho = None, peso = None, idade = None):
+        self.tipo = tipo
+        self.tamanho = tamanho
+        self.peso = peso
+        self.idade = idade
